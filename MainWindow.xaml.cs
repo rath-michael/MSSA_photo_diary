6368a2302d31e8262438c2009d69e82ef5025ef8
Michael <michael_rath0@outlook.com>
2022-09-01 17:20:01 -0700

    Removed test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace Week8Project
         public MainWindow()
         {
             InitializeComponent();
-            LoadDictionary();
+            //LoadDictionary();
         }
 
         private async void LoadDictionary()
